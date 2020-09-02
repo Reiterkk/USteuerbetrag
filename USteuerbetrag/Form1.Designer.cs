8ea9676bf4c4109e5e5fd5e6a345adaf713e259c
Kevin Reiter <kevin.reiter@kit.edu>
2020-09-02 17:04:26 +0200

    Berechnung des Steuerbetrags nach Angabe des Gehalts implementiert.

--- USteuerbetrag/Form1.Designer.cs
+++ USteuerbetrag/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CmdBerechne = new System.Windows.Forms.Button();
+            this.LblGehalt = new System.Windows.Forms.Label();
+            this.LblSteuerbetrag = new System.Windows.Forms.Label();
+            this.TxtEingabe = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CmdBerechne
+            // 
+            this.CmdBerechne.Location = new System.Drawing.Point(21, 158);
+            this.CmdBerechne.Name = "CmdBerechne";
+            this.CmdBerechne.Size = new System.Drawing.Size(100, 38);
+            this.CmdBerechne.TabIndex = 0;
+            this.CmdBerechne.Text = "Berechne";
+            this.CmdBerechne.UseVisualStyleBackColor = true;
+            this.CmdBerechne.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LblGehalt
+            // 
+            this.LblGehalt.AutoSize = true;
+            this.LblGehalt.Location = new System.Drawing.Point(16, 53);
+            this.LblGehalt.Name = "LblGehalt";
+            this.LblGehalt.Size = new System.Drawing.Size(81, 25);
+            this.LblGehalt.TabIndex = 1;
+            this.LblGehalt.Text = "Gehalt:";
+            this.LblGehalt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblSteuerbetrag
+            // 
+            this.LblSteuerbetrag.AutoSize = true;
+            this.LblSteuerbetrag.Location = new System.Drawing.Point(147, 99);
+            this.LblSteuerbetrag.Name = "LblSteuerbetrag";
+            this.LblSteuerbetrag.Size = new System.Drawing.Size(0, 25);
+            this.LblSteuerbetrag.TabIndex = 2;
+            // 
+            // TxtEingabe
+            // 
+            this.TxtEingabe.Location = new System.Drawing.Point(21, 93);
+            this.TxtEingabe.Name = "TxtEingabe";
+            this.TxtEingabe.Size = new System.Drawing.Size(100, 31);
+            this.TxtEingabe.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtEingabe);
+            this.Controls.Add(this.LblSteuerbetrag);
+            this.Controls.Add(this.LblGehalt);
+            this.Controls.Add(this.CmdBerechne);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CmdBerechne;
+        private System.Windows.Forms.Label LblGehalt;
+        private System.Windows.Forms.Label LblSteuerbetrag;
+        private System.Windows.Forms.TextBox TxtEingabe;
     }
 }
 
--- USteuerbetrag/Form1.cs
+++ USteuerbetrag/Form1.cs
@@ -16,5 +16,43 @@ namespace USteuerbetrag
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gehalt;
+            double faktor;
+            double steuerbetrag;
+            bool isParsable = Int32.TryParse(TxtEingabe.Text, out gehalt);
+            if (isParsable)
+            {
+                if (gehalt <= 12000)
+                {
+                    faktor = 0.12;
+                }
+                else if (gehalt <= 20000)
+                {
+                    faktor = 0.15;
+                }
+                else if (gehalt <= 30000)
+                {
+                    faktor = 0.20;
+                }
+                else
+                {
+                    faktor = 0.25;
+                }
+                steuerbetrag = gehalt * faktor;
+
+                LblSteuerbetrag.Text = steuerbetrag.ToString();
+            }
+
+            else
+                LblSteuerbetrag.Text = "Kein gültiges Gehalt angegeben!";
+        }
     }
 }
